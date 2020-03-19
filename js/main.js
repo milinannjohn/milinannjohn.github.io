1eeadcf444500119302b74e324337cbad9a18c43
milinannjohn <milin_b170009ar@nitc.ac.in>
2020-03-19 19:23:53 +0530

    Added form 2

--- js/main.js
+++ js/main.js
@@ -28,38 +28,38 @@
       });
     }
   }
-  $(function() {
-    $("#contact-form").validator();
-    $("#contact-form").on("submit", function(e) {
-      if (!e.isDefaultPrevented()) {
-        var url = "https://formspree.io/xzbvvnwr";
-        $.ajax({
-          type: "POST",
-          url: url,
-          data: $(this).serialize(),
-          success: function(data) {
-            var messageAlert = "alert-" + data.type;
-            var messageText = data.message;
-            var alertBox =
-              '<div class="alert ' +
-              messageAlert +
-              ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-              messageText +
-              "</div>";
-            if (messageAlert && messageText) {
-              $("#contact-form")
-                .find(".messages")
-                .html(alertBox);
-              if (messageAlert == "alert-success") {
-                $("#contact-form")[0].reset();
-              }
-            }
-          }
-        });
-        return false;
-      }
-    });
-  });
+  // $(function() {
+  //   $("#contact-form").validator();
+  //   $("#contact-form").on("submit", function(e) {
+  //     if (!e.isDefaultPrevented()) {
+  //       var url = "https://formspree.io/xzbvvnwr";
+  //       $.ajax({
+  //         type: "POST",
+  //         url: url,
+  //         data: $(this).serialize(),
+  //         success: function(data) {
+  //           var messageAlert = "alert-" + data.type;
+  //           var messageText = data.message;
+  //           var alertBox =
+  //             '<div class="alert ' +
+  //             messageAlert +
+  //             ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+  //             messageText +
+  //             "</div>";
+  //           if (messageAlert && messageText) {
+  //             $("#contact-form")
+  //               .find(".messages")
+  //               .html(alertBox);
+  //             if (messageAlert == "alert-success") {
+  //               $("#contact-form")[0].reset();
+  //             }
+  //           }
+  //         }
+  //       });
+  //       return false;
+  //     }
+  //   });
+  // });
   function mobileMenuHide() {
     var windowWidth = $(window).width();
     if (windowWidth < 1024) {
